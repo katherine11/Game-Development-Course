f971faeddef1b77fe4c8f11cb4a5d6407e7ef4dc
Katerina Simeonova <katherine.simeonova@gmail.com>
2019-06-07 00:24:42 +0300

    Add initial project

---
+++ Shuffle/android/src/com/ksimeonova/shuffle/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.ksimeonova.shuffle;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.ksimeonova.shuffle.Shuffle;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Shuffle(), config);
+	}
+}
---
+++ Shuffle/desktop/src/com/ksimeonova/shuffle/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.ksimeonova.shuffle.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.ksimeonova.shuffle.Shuffle;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Shuffle(), config);
+	}
+}
