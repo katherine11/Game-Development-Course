26772240eb1a4aef27d1665088e849690e635481
Katerina Simeonova <katherine.simeonova@gmail.com>
2019-06-10 19:51:28 +0300

    Add the player to the game screen

---
+++ Shuffle/core/src/com/ksimeonova/shuffle/model/Player.java
@@ -0,0 +1,57 @@
+package com.ksimeonova.shuffle.model;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
+import com.badlogic.gdx.physics.box2d.World;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.ksimeonova.shuffle.Shuffle;
+
+public class Player extends Image {
+
+    private Shuffle shuffle;
+    private World physicalWorld;
+    private Body body;
+
+    public Player(Shuffle shuffle, World physicalWorld, float x, float y, float size, Texture texture){
+        super(texture);
+        this.shuffle = shuffle;
+        this.physicalWorld = physicalWorld;
+        setPosition(x, y);
+        setSize(size, size);
+        setOrigin(x, y);
+        initBody();
+    }
+
+    private void initBody(){
+        BodyDef bodyDef = new BodyDef();
+        bodyDef.position.set(getX(),getY());
+        bodyDef.type = BodyDef.BodyType.DynamicBody;
+
+        body = physicalWorld.createBody(bodyDef);
+
+        PolygonShape bodyShape = new PolygonShape();
+        bodyShape.setAsBox(getWidth() / 2,getHeight() / 2);
+
+        FixtureDef fixtureDef = new FixtureDef();
+        fixtureDef.shape = bodyShape;
+        fixtureDef.density = 0.5f;
+        fixtureDef.friction = 0.5f;
+        fixtureDef.restitution = 0.5f;
+
+        body.createFixture(fixtureDef);
+        body.setUserData(this);
+
+        bodyShape.dispose();
+
+    }
+
+    @Override
+    public void act(float delta) {
+        this.setPosition(body.getPosition().x - getWidth() / 2,body.getPosition().y - getHeight() / 2);
+        this.setRotation(body.getAngle() * MathUtils.radiansToDegrees);
+    }
+}
--- Shuffle/core/src/com/ksimeonova/shuffle/screen/GameScreen.java
+++ Shuffle/core/src/com/ksimeonova/shuffle/screen/GameScreen.java
@@ -3,42 +3,48 @@ package com.ksimeonova.shuffle.screen;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.World;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.utils.viewport.StretchViewport;
 import com.ksimeonova.shuffle.Shuffle;
+import com.ksimeonova.shuffle.model.Player;
 
 public class GameScreen implements Screen {
 
     private Shuffle shuffle;
-    private SpriteBatch batch;
-    private OrthographicCamera camera;
-    private Texture img;
+    private Stage stage;
+    private World physicalWorld;
+    private Player player;
+    private Texture playerImage;
+    private float worldWidth;
 
-    public GameScreen(Shuffle shuffle){
+    public GameScreen(Shuffle shuffle) {
         this.shuffle = shuffle;
     }
 
     @Override
     public void show() {
-        batch = new SpriteBatch();
+        playerImage = new Texture("Inner_Sakura.png");
+        physicalWorld = new World(new Vector2(0, -0.5f), false);
+        this.player = new Player(shuffle, physicalWorld, 5, Shuffle.WORLD_HEIGHT / 2, 5, playerImage);
 
-        camera = new OrthographicCamera();
-        camera.setToOrtho(false, Shuffle.WIDTH, Shuffle.HEIGHT);
-        camera.update();
+        float ratio = (float) Gdx.graphics.getHeight() / (float) Gdx.graphics.getWidth();
+        this.worldWidth = Shuffle.WORLD_HEIGHT / ratio;
 
-        img = new Texture("Inner_Sakura.png");
+        this.stage = new Stage(new StretchViewport(worldWidth, Shuffle.WORLD_HEIGHT));
+        this.stage.addActor(player);
     }
 
     @Override
     public void render(float delta) {
-        Gdx.gl.glClearColor(153/255f, 102/255f, 1, 1);
+        Gdx.gl.glClearColor(153 / 255f, 102 / 255f, 1, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        batch.setProjectionMatrix(camera.combined);
-        camera.update();
-        batch.begin();
-        batch.draw(img, 0, 0);
-        batch.end();
+
+        physicalWorld.step(Gdx.graphics.getDeltaTime(), 6, 2);
+        stage.act();
+        stage.draw();
     }
 
     @Override
@@ -63,7 +69,6 @@ public class GameScreen implements Screen {
 
     @Override
     public void dispose() {
-        batch.dispose();
-        img.dispose();
+        playerImage.dispose();
     }
 }
