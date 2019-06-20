da043bb03e4a3a915df3e3cfc928cf31163bd6f4
Katerina Simeonova <katherine.simeonova@gmail.com>
2019-06-20 08:58:13 +0300

    Change background color

--- Shuffle/core/src/com/ksimeonova/shuffle/screen/GameScreen.java
+++ Shuffle/core/src/com/ksimeonova/shuffle/screen/GameScreen.java
@@ -47,7 +47,7 @@ public class GameScreen implements Screen {
 
     @Override
     public void render(float delta) {
-        Gdx.gl.glClearColor(153 / 255f, 102 / 255f, 1, 1);
+        Gdx.gl.glClearColor(255 / 255f, 204/ 255f, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
         physicalWorld.step(Gdx.graphics.getDeltaTime(), 6, 2);
