70bb763eee409aee62571f8e6254e69a1ecb9350
Katerina Simeonova <katherine.simeonova@gmail.com>
2019-06-21 09:28:02 +0300

    Extract constants

--- Shuffle/core/src/com/ksimeonova/shuffle/model/EnemyColumn.java
+++ Shuffle/core/src/com/ksimeonova/shuffle/model/EnemyColumn.java
@@ -10,7 +10,6 @@ import com.ksimeonova.shuffle.Shuffle;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.Random;
 
 import static com.ksimeonova.shuffle.game.GameWorld.GAME_COLORS;
 
@@ -19,8 +18,7 @@ public class EnemyColumn {
     public static final String ENEMY_IMAGE_NAME = "column.png";
     public static final float ENEMY_COLUMN_WIDTH = 0.5f;
     public static final int ENEMY_COLUMN_HEIGHT = 4;
-    public static final int ENEMY_COLUMNS_NUMBER = 4;
-    public static final int ENEMY_COLUMNS_COUNT = 5;
+    public static final int ENEMY_COLUMNS_COUNT = 4;
 
     private Shuffle shuffle;
     private World physicalWorld;
@@ -39,9 +37,9 @@ public class EnemyColumn {
     private void initEnemies() {
         this.enemies = new ArrayList<Enemy>();
         Collections.shuffle(GAME_COLORS);
-        for (int i = 0; i < 4; i++) {
+        for (int i = 0; i < ENEMY_COLUMNS_COUNT; i++) {
             Texture texture = new Texture(ENEMY_IMAGE_NAME);
-            Enemy enemy = new Enemy(shuffle, physicalWorld, x, i * 4 + 2,
+            Enemy enemy = new Enemy(shuffle, physicalWorld, x, i * ENEMY_COLUMN_HEIGHT + ENEMY_COLUMN_HEIGHT / 2,
                     ENEMY_COLUMN_WIDTH, ENEMY_COLUMN_HEIGHT, texture, BodyDef.BodyType.StaticBody);
             Color color = GAME_COLORS.get(i);
             enemy.setColor(color);
